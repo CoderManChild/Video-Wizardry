fdbe69767a952972d0adb4ee80fca8f5b656c7ce
aaryan-k-lab <66313584+aaryan-k-lab@users.noreply.github.com>
2023-10-14 14:52:39 -0400

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-
+let transcript;
 
 function animateContainers() {
             // Hide the text-container
@@ -19,3 +19,29 @@ function animateButtonContainer() {
             const button3Container = document.querySelector('.button-3-container');
             button3Container.style.display = 'flex';
          }
+
+
+async function getTranscript() {
+    const videoUrl = document.getElementById("url-textbox").value;
+    try {
+        // Get the video ID from the URL
+        const videoId = videoUrl.split('v=')[1].split('&')[0];
+
+        // Get the transcript data
+        const transcriptData = await YouTubeTranscriptApi.getTranscript(videoId);
+
+        // Extract the text content from the transcript data
+        const transcript = transcriptData.map(entry => entry.text).join('\n');
+
+        console.log('Transcript retrieved successfully.');
+        return transcript;
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        return null;
+    }
+}
+
+function processAndAnimate() {
+    getTranscript();
+    animateContainers();
+}
