3c42fa801fb839d47ab725e6d30b546b1cd80b65
CoderManChild <130810033+CoderManChild@users.noreply.github.com>
2023-10-14 11:20:33 -0400

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -1,6 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-def download_transcript(video_url, output_file):
+def get_transcript(video_url):
     try:
         # Get the video ID from the URL
         video_id = video_url.split('v=')[1]
@@ -8,18 +8,13 @@ def download_transcript(video_url, output_file):
         # Download the transcript
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-        # Create a text file to save the transcript
-        with open(output_file, 'w', encoding='utf-8') as file:
-            for entry in transcript:
-                text = entry['text']
-                file.write(text + '\n')
-
         print('Transcript downloaded successfully.')
+        return transcript
 
     except Exception as e:
         print(f'Error: {e}')
+        return None
 
 video_url = 'https://www.youtube.com/watch?v=6G6i8vSa8Zs&ab_channel=TheOrganicChemistryTutor'
-output_file = 'transcript.txt'
 
-download_transcript(video_url, output_file)
+transcript = get_transcript(video_url)
