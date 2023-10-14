3a2fc445e9495327eadff10cf51b1e25a36592fd
CoderManChild <130810033+CoderManChild@users.noreply.github.com>
2023-10-14 15:22:39 -0400

    Update transcript.js

--- transcript.js
+++ transcript.js
@@ -1,27 +1,7 @@
-const YouTubeTranscriptApi = require('youtube-transcript-api');
+const { YoutubeTranscript } = require('youtube-transcript');
 
-async function getTranscript(videoUrl) {
-    try {
-        // Get the video ID from the URL
-        const videoId = videoUrl.split('v=')[1].split('&')[0];
-
-        // Get the transcript data
-        const transcriptData = await YouTubeTranscriptApi.getTranscript(videoId);
-
-        // Extract the text content from the transcript data
-        const transcript = transcriptData.map(entry => entry.text).join('\n');
-
-        console.log('Transcript retrieved successfully.');
-        return transcript;
-    } catch (error) {
-        console.error(`Error: ${error}`);
-        return null;
-    }
-}
-
-// Replace 'YOUR_VIDEO_URL' with the URL of the YouTube video you want to retrieve the transcript for
-const videoUrl = 'https://www.youtube.com/watch?v=6G6i8vSa8Zs&ab_channel=TheOrganicChemistryTutor';
-
-getTranscript(videoUrl).then((transcript) => {
-    console.log(transcript);
+YoutubeTranscript.fetchTranscript('https://www.youtube.com/watch?v=aFfmMZ7AQzs&ab_channel=DharMannStudios').then((transcript) => {
+    transcript.forEach((entry, index) => {
+        console.log(`${entry.text}`);
+    });
 });
